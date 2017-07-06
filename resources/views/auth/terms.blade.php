69b46a1366d779885169174cacc32b245d95f123
frantanza <frcmarti@alumnos.ubiobio.cl>
2017-07-06 17:00:16 -0400

    Terminos y condiciones Universidad de Macondo

--- resources/views/auth/terms.blade.php
+++ resources/views/auth/terms.blade.php
@@ -3,23 +3,24 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">Terminos y Condiciones</h3>
+                <h3 class="modal-title">Terminos y Condiciones Universidad De Macondo</h3>
             </div>
 
             <div class="modal-body">
 <p>
-1.<strong>Derechos de Propiedad.</strong> Entre usted y Time Inc., Time Inc. es dueño único y exclusivo, de todos los derechos, título e intereses en y del Sitio Web, de todo el contenido (incluyendo, por ejemplo, audio, fotografías, ilustraciones, gráficos, otros medios visuales, videos, copias, textos, software, títulos, archivos de Onda de choque, etc.) 
+1.<strong>Derechos de Propiedad.</strong> Universidas De Macondo Ltda Es dueño único y exclusivo, de todos los derechos, título e intereses en y del Sitio Web, de todo el contenido (incluyendo, por ejemplo, El registro de los Convenios, La asociacion de Instituciones, Registro de coordinadores, El registro de Instituciones asociadas a la Universidad de Macando Ltda, medios visuales, videos, copias, textos, títulos). 
 </p>
 <p>
-2.<strong>Licencia Limitada.</strong> Usted puede acceder y ver el contenido del Sitio Web desde su computadora o desde cualquier otro aparato y, a menos de que se indique de otra manera en estos Términos y Condiciones o en el Sitio Web, sacar copias o impresiones individuales del contenido del Sitio Web para su uso personal, interno únicamente. El uso del Sitio Web y de los servicios que se ofrecen en o a través del Sitio Web, son sólo para su uso personal, no comercial.
+2.<strong>Licencia Limitada.</strong> Usted puede acceder y ver el contenido del Sitio Web desde su computadora o desde cualquier otro aparato y, Usted podra sacar copias o impresiones individuales del contenido del Sitio Web para uso exclusivo profesional, interno únicamente. El uso del Sitio Web y de los servicios que se ofrecen en o a través del Sitio Web, son sólo para el uso de usuarios debidamente registrados y validados en el sistema, no comercial.
 </p>
 <p>
-3.<strong>Uso Prohibido.</strong> Cualquier distribución, publicación o explotación comercial o promocional del Sitio Web, o de cualquiera de los contenidos, códigos, datos o materiales en el Sitio Web, está estrictamente prohibida, a menos de que usted haya recibido el previo permiso expreso por escrito del personal autorizado de Time Inc. o de algún otro poseedor de derechos aplicable.
+3.<strong>Uso Prohibido.</strong> Cualquier distribución de la información, bases de datos, contratos, licencias y cualquier informacion personal de usuarios y clientes del sistema de Seguimiento de Convenios.
+El quebramiento de esta norma y exigencia llevara demanda judicial por mal uso de la información  
 </p>
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">OK</button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Lei Terminos Universidad de Macondo</button>
             </div>
         </div>
     </div>
